48b7d1403adfcfd88af484906ff41a3c3c665eb7
Gardian <gardianrahman91@gmail.com>
2025-05-08 18:45:34 +0700

    Melengkapi class LinkedList

--- singleList.cpp
+++ singleList.cpp
@@ -41,5 +41,19 @@ class LinkedList
         
         Node *previous = START;
         Node *current = START;
+
+        while ((current != NULL) && (nim >= current ->noMhs))
+        {
+            if (nim == current -> noMhs)
+            {
+                cout << "\nDuplikasi noMhs tidak diijinkan\n";
+                return;
+            }
+            previous = current;
+            current = current->next;            
+        }
+        
+        nodeBaru->next = current;
+        previous->next = nodeBaru;
     }
 }
