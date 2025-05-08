4b1c60882f1792d4d281ed3c4de4cd6c276959a0
Gardian <gardianrahman91@gmail.com>
2025-05-08 19:36:52 +0700

    Case 1

--- singleList.cpp
+++ singleList.cpp
@@ -129,6 +129,15 @@ int main()
       cout << endl
             << "Masukkan pilihan (1-5): ";
             cin >> ch;
+            switch (ch)
+            {
+                case '1':
+                {
+                    mhs.addNode();
+                    break;
+                }
+            }
     } 
+    
     
 }
